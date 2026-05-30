79353341d3fda51dbe37e25e089608fa5a9a6b36
iknahar <knahar.kamrun@gmail.com>
2026-05-31 00:02:41 +0600

    Bump forge to 1.0.1

--- Casks/forge.rb
+++ Casks/forge.rb
@@ -1,6 +1,6 @@
 cask "forge" do
-  version "1.0.0"
-  sha256 "3fabc63ecc3590365e68650842623dc1428e9f8096333d3163fdb11abf1b1cf6"
+  version "1.0.1"
+  sha256 "a0f77f0c0a56b3b125d4fff8edfd3a0e504559c3ae789b3d4708258805b69375"
 
   url "https://github.com/iknahar/forge-app-source-code/releases/download/v#{version}/Forge-#{version}.dmg"
   name "Forge"
