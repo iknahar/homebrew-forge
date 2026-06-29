5b7480b1b8d4edfcf92b8ab0dde63725fbbaed74
iknahar <knahar.kamrun@gmail.com>
2026-06-29 14:40:37 +0600

    forge 1.0.16

--- Casks/forge.rb
+++ Casks/forge.rb
@@ -1,6 +1,6 @@
 cask "forge" do
-  version "1.0.15"
-  sha256 "c4a397b49e79318287f32d6c641d50bb34d10ead9e1c71f3e3fd47e6b6b0ac80"
+  version "1.0.16"
+  sha256 "357839332f494f12afd3a2eb3145de6a48ad43ec475c4764982a761fddd4589c"
 
   url "https://github.com/iknahar/forge-app-source-code/releases/download/v#{version}/Forge-#{version}.dmg"
   name "Forge"
